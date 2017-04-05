c89bebd7179d102e92ce7ffec3e8635091f1b8c8
Kent Daleng <kentsd16@student.uia.no>
2017-04-05 08:35:18 +0000

    Change WindGust fields from float64 to string

--- weather/wunderground.go
+++ weather/wunderground.go
@@ -83,10 +83,10 @@ type WundergroundData struct {
 		WindDir string `json:"wind_dir"`
 		WindDegrees float64 `json:"wind_degrees"`
 		WindMph float64 `json:"wind_mph"`
-		WindGustMph float64 `json:"wind_gust_mph"`
+		WindGustMph string `json:"wind_gust_mph"`
 		WindKph float64 `json:"wind_kph"`
         WindMs float64 // This is not a field in json, but will be added later.
-		WindGustKph float64 `json:"wind_gust_kph"`
+		WindGustKph string `json:"wind_gust_kph"`
 		PressureMb string `json:"pressure_mb"`
 		PressureIn string `json:"pressure_in"`
 		PressureTrend string `json:"pressure_trend"`
